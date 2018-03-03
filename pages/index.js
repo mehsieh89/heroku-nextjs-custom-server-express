7510d3491e0eb23d222da8d402eea7c897eb4714
Mel hsieh <mehsieh89@gmail.com>
2018-03-03 04:27:59 -0800

    testing all parameters

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Test from '../components/test.jsx';
 import Link from 'next/link'
 
 export default () => (
   <ul>
     <li><Link href='/b' as='/a'><a>a</a></Link></li>
     <li><Link href='/a' as='/b'><a>b</a></Link></li>
+    <Test/>
   </ul>
 )
