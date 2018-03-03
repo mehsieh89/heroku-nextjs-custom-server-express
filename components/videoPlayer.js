c4db432e05386898cc8c5b007cec9bb4524f45e0
Mel hsieh <mehsieh89@gmail.com>
2018-03-03 07:39:39 -0800

    removing css

--- components/videoPlayer.js
+++ components/videoPlayer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import custom from "../styles/custom.css";
 import CommentsContainer from "../containers/commentsContainer.js";
 
 class VideoPlayer extends Component {
